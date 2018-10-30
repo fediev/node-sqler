7b4e02e0655f2f6516a8d39e83d0959959b4c63c
fediev <fediev@gmail.com>
2018-10-30 11:41:52 +0900

    Add lib and test skeleton

---
+++ tests/sqler-helper.test.js
@@ -0,0 +1,8 @@
+/* eslint-disable prefer-arrow-callback, func-names */
+const helper = require('../lib/sqler-helper');
+
+describe('sqler-helper', function() {
+  it('sqler-helper test', function() {
+    console.info(helper);
+  });
+});
---
+++ tests/sqler.test.js
@@ -0,0 +1,8 @@
+/* eslint-disable prefer-arrow-callback, func-names */
+const sqler = require('../lib/sqler');
+
+describe('sqler', function() {
+  it('sqler test', function() {
+    console.info(sqler);
+  });
+});
