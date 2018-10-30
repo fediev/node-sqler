29f0ddc2ff80b9cbba9ffaeabb16d9b58c273c69
fediev <fediev@gmail.com>
2018-10-30 14:53:59 +0900

    Add select() limit tests
    implementation of select() is partial, just for testing limit

--- tests/sqler.test.js
+++ tests/sqler.test.js
@@ -1,8 +1,69 @@
 /* eslint-disable prefer-arrow-callback, func-names */
-const sqler = require('../lib/sqler');
+const { expect } = require('chai');
+const { select } = require('../lib/sqler');
 
 describe('sqler', function() {
-  it('sqler test', function() {
-    console.info(sqler);
+  describe('select()', function() {
+    const tests = [
+      // [description, limit expression, expected result]
+
+      // LIMIT clause examples
+      [
+        'string -> LIMIT 1, 2',
+        {
+          tb: 'tb1',
+          limit: '  1, 2   ',
+        },
+        'SELECT * FROM tb1 LIMIT 1, 2',
+      ],
+      [
+        'number -> LIMIT 1',
+        {
+          tb: 'tb1',
+          limit: 1,
+        },
+        'SELECT * FROM tb1 LIMIT 1',
+      ],
+      [
+        'array [1] -> LIMIT 1',
+        {
+          tb: 'tb1',
+          limit: [1],
+        },
+        'SELECT * FROM tb1 LIMIT 1',
+      ],
+
+      [
+        'array [1, 2] -> LIMIT 1, 2',
+        {
+          tb: 'tb1',
+          limit: [1, 2],
+        },
+        'SELECT * FROM tb1 LIMIT 1, 2',
+      ],
+      [
+        'object { count: 1 } -> LIMIT 1',
+        {
+          tb: 'tb1',
+          limit: { count: 1 },
+        },
+        'SELECT * FROM tb1 LIMIT 1',
+      ],
+      [
+        'object { count: 1, offset: 2 } -> LIMIT 1, 2',
+        {
+          tb: 'tb1',
+          limit: { count: 1, offset: 2 },
+        },
+        'SELECT * FROM tb1 LIMIT 1 OFFSET 2',
+      ],
+    ];
+
+    tests.forEach(function([desc, queryOption, expected]) {
+      it(desc, function() {
+        const result = select(queryOption);
+        expect(result).to.eq(expected);
+      });
+    });
   });
 });
